9da10b154adf2447a26cf55e57205d69728381b7
XBDHJG <129221851+XBDHJG@users.noreply.github.com>
2024-06-03 23:41:10 +0300

    Update songs.py

--- AnonXMusic/plugins/play/songs.py
+++ AnonXMusic/plugins/play/songs.py
@@ -16,7 +16,7 @@ import asyncio
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
-from AdRenalen import app
+from AnonXMusic import app
 
 
 @app.on_message(filters.command(["اغاني","الفنان","✨اغاني"], ""))
